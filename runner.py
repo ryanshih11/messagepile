623c745ffb8392a616b117a8af2d8ad9d1421274
CMatri <cperkin4@gmu.edu>
2020-09-11 01:54:49 -0400

    Java working, have to define class as Main though - to be fixed

--- runner.py
+++ runner.py
@@ -1,4 +1,5 @@
 import os
+import threading
 import subprocess
 
 class Runner():
@@ -15,3 +16,15 @@ class Runner():
     def python(self):
         p = subprocess.Popen(['/usr/bin/python', self.code_file], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         return p.communicate()
+    
+    def java_cleanup_thread(self):
+        os.system('sleep 5; rm /tmp/Main.java /tmp/Main.class')
+
+    def java(self):
+        os.system('cp /tmp/code /tmp/Main.java; javac /tmp/Main.java')
+        env = os.environ.copy()
+        env['CLASSPATH'] = '/tmp'
+        p = subprocess.Popen(['/usr/bin/java', 'Main'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, env=env)
+        threading.Thread(target=self.java_cleanup_thread).start()
+        return p.communicate()
+        
