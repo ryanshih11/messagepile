8e3902b369a2c0af27170c7b88affa7cdff327bc
CMatri <cperkin4@gmu.edu>
2020-09-11 02:43:31 -0400

    Added rust

--- runner.py
+++ runner.py
@@ -40,6 +40,7 @@ class Runner():
 
     def go_cleanup_thread(self):
         os.system('sleep 5; rm -rf /tmp/src')
+        return "Look at you poking around", "Hmmmm"
 
     def go(self):
         os.system('mkdir /tmp/src; mkdir /tmp/src/code; cp /tmp/code /tmp/src/code/code.go')
@@ -48,4 +49,14 @@ class Runner():
         p = subprocess.Popen(['/usr/bin/go', 'run', 'code'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, env=env)
         threading.Thread(target=self.go_cleanup_thread).start()
         return p.communicate()
-        
+    
+    def rust_cleanup_thread(self):
+        os.system('sleep 5; rm /tmp/a.out')
+        return "Look at you poking around", "Hmmmm"
+
+    def rust(self):
+        compile_proc = subprocess.Popen(['/usr/bin/rustc', self.code_file, '-o', '/tmp/a.out'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        os.system('sleep 5')
+        p = subprocess.Popen(['/tmp/a.out'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+        threading.Thread(target=self.rust_cleanup_thread).start()
+        return p.communicate()
