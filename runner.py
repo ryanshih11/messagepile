aef49692a5112b60e6663e251d40c92209ae6708
CMatri <cperkin4@gmu.edu>
2020-09-12 00:24:06 -0400

    Dynamic cleanup threading and reduction of code duplication.

--- runner.py
+++ runner.py
@@ -1,4 +1,5 @@
 import os
+import inspect
 import threading
 import subprocess
 
@@ -13,11 +14,17 @@ class Runner():
         os.remove(self.code_file)
         return result
 
+    def generic_execute(self, command, args, env=os.environ.copy()):
+        calframe_cleanup_func = getattr(self, inspect.getouterframes(inspect.currentframe(), 2)[1][3] + '_cleanup_thread')
+        command.extend(args)
+        p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, env=env)
+        if calframe_cleanup_func is not None:
+            threading.Thread(target=calframe_cleanup_func).start()
+        return p.communicate()
+        
     def generic_interpreter(self, path, args):
         command = [path, self.code_file]
-        command.extend(args)
-        p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-        return p.communicate()
+        return self.generic_execute(command, args)
     
     def python(self, args):
         return self.generic_interpreter('/usr/bin/python', args)
@@ -31,10 +38,7 @@ class Runner():
         env = os.environ.copy()
         env['CLASSPATH'] = '/tmp'
         command = ['/usr/bin/java', 'Main']
-        command.extend(args)
-        p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, env=env)
-        threading.Thread(target=self.java_cleanup_thread).start()
-        return p.communicate()
+        return self.generic_execute(command, args, env)
 
     def brainfuck(self, args):
         return self.generic_interpreter('/usr/bin/brainfuck', args)
@@ -49,11 +53,8 @@ class Runner():
     def go(self, args):
         os.system('cp /tmp/code /tmp/code.go; export GOPATH=/tmp; cd /tmp; go build -o /tmp/codego')
         command = ['/tmp/codego']
-        command.extend(args)
-        p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-        #threading.Thread(target=self.go_cleanup_thread).start()
-        return p.communicate()
-    
+        return self.generic_execute(command, args)
+
     def rust_cleanup_thread(self):
         os.system('sleep 5; rm /tmp/a.out')
         return "Look at you poking around", "Hmmmm"
@@ -62,7 +63,13 @@ class Runner():
         compile_proc = subprocess.Popen(['/usr/bin/rustc', self.code_file, '-o', '/tmp/a.out'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         os.system('sleep 5')
         command = ['/tmp/a.out']
-        command.extend(args)
-        p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-        threading.Thread(target=self.rust_cleanup_thread).start()
-        return p.communicate()
+        return self.generic_execute(command, args)
+
+    def csharp_cleanup_thread(self):
+        os.system('rm /tmp/code.exe')
+        return "Look at you poking around", "Hmmmm"
+
+    def csharp(self, args):
+        os.system('mcs /tmp/code')
+        command = ['/usr/bin/mono', '/tmp/code.exe']
+        return self.generic_execute(command, args)
